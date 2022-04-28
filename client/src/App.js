64d00107bac7f0c63e6f2ebfeecb7fb2e77ad615
Chris Castle <chrisjcastle93@gmail.com>
2022-04-28 23:08:59 +0200

    IBM API Working

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  // curl -X POST -u "apikey:Tsp6V8lAz5Pkw95z7NubBWRiv9Tfg4ciTBdHAPlWIKrx" --header "Content-Type: audio/flac" --data-binary "@audio-file.flac" "https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/d64c60a7-5eeb-4ce4-a321-e3c0bcef6f3c/v1/recognize"
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,12 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,8 +1,31 @@
 const router = require("express").Router();
 const authRoutes = require("./auth.routes");
 
+const fs = require("fs");
+const SpeechToTextV1 = require("ibm-watson/speech-to-text/v1");
+const { IamAuthenticator } = require("ibm-watson/auth");
+
+const speechToText = new SpeechToTextV1({
+  authenticator: new IamAuthenticator({ apikey: "Tsp6V8lAz5Pkw95z7NubBWRiv9Tfg4ciTBdHAPlWIKrx" }),
+  serviceUrl: "https://api.us-south.speech-to-text.watson.cloud.ibm.com",
+});
+
+const params = {
+  // From file
+  audio: fs.createReadStream("./../test.wav"),
+  contentType: "audio/l16; rate=44100",
+};
+
 /* GET home page */
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
+  speechToText
+    .recognize(params)
+    .then((response) => {
+      console.log(JSON.stringify(response.result, null, 2));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   res.json("All good in here");
 });
 
