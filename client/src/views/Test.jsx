fcae6262149008791ddbe98d1be12a2349844e03
Chris Castle <chrisjcastle93@gmail.com>
2022-04-30 21:52:11 +0200

    refactored STT on client and server

--- client/src/views/Test.jsx
+++ client/src/views/Test.jsx
@@ -1,73 +1,54 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { speechToTextService } from "../services/speechToText";
 
 export default function Test() {
-  let [audioRecording, setAudioRecording] = useState("");
   let [speechToTextResult, setSpeechToTextResult] = useState("");
-  let [mediaRecorder, setMediaRecorder] = useState({});
 
-  const handleMicFormSubmit = (e) => {
-    e.preventDefault();
-    navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(handleSuccess);
+  const recordMicrophone = async (e) => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+    recordUserAudio(stream)
   };
 
-  const handleSuccess = (stream) => {
+  const recordUserAudio = (stream) => {
     const stopButton = document.getElementById("stop");
     const options = { mimeType: "audio/webm" };
     const recordedChunks = [];
     const mediaRecorder = new MediaRecorder(stream, options);
 
-    mediaRecorder.addEventListener("dataavailable", function (e) {
+    mediaRecorder.addEventListener("dataavailable", (e) => {
       if (e.data.size > 0) recordedChunks.push(e.data);
     });
 
-    mediaRecorder.addEventListener("stop", function () {
-      console.log("MEDIA RECORDER STOPPED");
+    mediaRecorder.addEventListener("stop", () => {
       stream.getTracks().forEach(function (track) {
         track.stop();
       });
       const recording = new Blob(recordedChunks, { type: "audio/webm" })
-    //   const recording = URL.createObjectURL(new Blob(recordedChunks, { type: "audio/webm" }));
-      console.log(recording.size)
-    //   setAudioRecording(recording);
-      sendAudioFile(recording);
+      sendAudioFileToBackend(recording);
     });
 
-    stopButton.addEventListener("click", function () {
-      console.log("STOP BUTTON IN FUNCTION");
+    stopButton.addEventListener("click", () => {
       mediaRecorder.stop();
     });
 
     mediaRecorder.start();
-
-    setMediaRecorder(mediaRecorder);
   };
 
-  const sendAudioFile = (file) => {
+  const sendAudioFileToBackend = async (file) => {
+
     const formData = new FormData();
     formData.append("file", file);
-    axios
-      .post("http://localhost:5005/api/test", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      })
-      .then((result) => setSpeechToTextResult(result.data))
-      .catch((err) => console.log(err));
+
+    const result = await speechToTextService.createNewSearch(formData)
+
+    setSpeechToTextResult(result.data)
+
   };
 
   return (
     <div>
-      <form onSubmit={(e) => handleMicFormSubmit(e)}>
-        <h1> MICROPHONE VERSION </h1>
-        <button type="submit">GET MIC ACCESS</button>
-      </form>
+      <button onClick={(e) => recordMicrophone(e)}>GET MIC ACCESS</button>
       <button id="stop">Stop</button>
-      <br />
-      <br />
-      <h1>PLAYER (STATE)</h1>
-      {/* <audio id="player" src={audioRecording} controls></audio> */}
-      <h1>RESULT</h1>
       {speechToTextResult ? <p>{speechToTextResult}</p> : <p> NO SPEECH RESULT YET </p>}
     </div>
   );
