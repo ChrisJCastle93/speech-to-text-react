b3387faea621e52f57553e823df147ab48334915
Chris Castle <chrisjcastle93@gmail.com>
2022-05-02 08:35:55 +0200

    set up amazon api and voice to search

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,31 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './views/Home'
-import Test from './views/Test'
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./views/Home";
+import Test from "./components/Microphone";
+import SearchContainer from "./views/SearchContainer";
+import SearchResults from "./views/SearchResults";
+import { ChakraProvider } from "@chakra-ui/react";
+import { useState } from "react";
 
 function App() {
+  let [searchResultsArray, setSearchResultsArray] = useState([]);
+
+  const handleSearchResults = (searchResults) => {
+    setSearchResultsArray(searchResults.data);
+  };
+
   return (
     <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={ <Home /> } />
-          <Route path="/test" element={ <Test  /> } />
-        </Routes>
-      </div>
+      <ChakraProvider>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/test" element={<Test />} />
+            <Route path="/search" element={<SearchContainer handleSearchResults={handleSearchResults} />} />
+            <Route path="/search/results" element={<SearchResults searchResultsArray={searchResultsArray} />} />
+          </Routes>
+        </div>
+      </ChakraProvider>
     </BrowserRouter>
   );
 }
