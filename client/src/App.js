5612c82187d09ec77a925c3f2f274a45e304b5ae
cschnitzel10 <boese.caroline@gmail.com>
2022-05-03 19:17:52 +0200

    some components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import {Navbar} from './components/Navbar';
+import Home from './components/Home';
+import SearchResults from './components/SearchResults';
+import ProductDetail from './components/ProductDetail';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const handleSearchResults =(data)=>{
+console.log("searchResults",data)
 }
 
+  return (
+  
+    <div>
+        <Navbar handleSearchResults={handleSearchResults}/>
+
+      <div>
+        <Routes>
+        <Route path="/" element={<Home />} />
+          <Route path="/search/:?" element={<SearchResults />} />
+          <Route path="/search/:product._id" element={<ProductDetail />} />
+        </Routes>
+      </div>
+      <footer>
+        <p>This is made by Anabelle, Chris and Caro</p>
+      </footer> 
+    </div> 
+
+  );
+}
 export default App;
--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const authRoutes = require("./auth.routes");
+const search = require("./search.routes");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
-
 router.use("/auth", authRoutes);
 
+router.use("/", search);
 module.exports = router;
