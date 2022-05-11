fe0e87342b1579bbc337b18542ae5006868029bd
Chris Castle <chrisjcastle93@gmail.com>
2022-05-11 06:29:45 +0200

    heroku

--- README.md
+++
@@ -1,7 +0,0 @@
-# speech-to-text-react
- // very important text
-
- // 1. function to get search string state
-//2. we send the function to navbar and get the search string
-//3. we send the function to search result and
-//4. we get the string and do an axios call in backend (in search result)
--- server/server.js
+++ server/server.js
@@ -3,6 +3,13 @@ const app = require("./app");
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 5005;
 
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
 });
