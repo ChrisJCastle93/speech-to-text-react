d88068d38211cb1fdfe50381049d30ebe5a0e6b0
Annabelle Peacock <aspeacock92@gmail.com>
2022-05-14 19:21:11 +0200

    fixed search functionality, more styling needed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import AuthButtonDisplay from './AuthButtonDisplay';
-// import SearchContainer from './search/SearchContainer';
+import SearchContainer from './search/SearchContainer';
 import Microphone from './search/Microphone';
 import SearchBar from './search/SearchBar';
 import "./navbar.css";
 
 
-export default function Navbar({loggedInUser, logoutHandler, searchTerm, searchHandler, handleSubmit, updateSpeechDone}) {
+export default function Navbar({loggedInUser, logoutHandler, handleSearchResults}) {
   return (
     <div className="navbar">
         <AuthButtonDisplay   
             loggedInUser={loggedInUser}
             logoutHandler={logoutHandler}
             />
-      <div className='search'>
+      {/* <div className='search'>
           <Microphone searchHandler={searchHandler} handleSubmit={handleSubmit} updateSpeechDone={updateSpeechDone} />
           <SearchBar searchTerm={searchTerm} searchHandler={searchHandler} handleSubmit={handleSubmit} />
-          </div>
-        {/* <SearchContainer
+          </div> */}
+        <SearchContainer
           handleSearchResults={handleSearchResults}
-         /> */}
+         />
         <Link to="/profile/edit" className="auth-btn">edit profile</Link>
 
     </div>
