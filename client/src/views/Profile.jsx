1ce5324682aaeb73123cbbd692d1c34027e14960
Annabelle Peacock <aspeacock92@gmail.com>
2022-05-19 22:43:55 +0200

    navbar height fixed, styling for auth and profile pages

--- client/src/views/Profile.jsx
+++ client/src/views/Profile.jsx
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import { CartItem } from "../components/cart/CartItem";
 import { cartService } from "../services/localStorage";
 import { NavLink } from 'react-router-dom';
+import paris from "../assets/paris.png";
+
 import "../css/profile.css";
 
 export default function Profile (props) {
@@ -29,15 +31,18 @@ export default function Profile (props) {
 
   return (
     <div>
-      <h1>Hi {props.loggedInUser?.username}</h1>
-      <h2>Your wishlist</h2>
+      <h1 className="profile-header">Get a light, {props.loggedInUser?.username}</h1>
+
+      {/* <h3 className="profile-p">Scroll down for wishlist</h3> */}
+      <img className="auth-img" src ={paris} alt="lamp-setting" />
+      <h2 className="profile-header-1">Your wishlist</h2>
 
       <div className="wishlist">
           {wishlistData?.map((item) => (
                   <CartItem key={item.id} {...item} onClickDelete={onClickDelete} isWishList noQuantity/>
                 ))}
       </div>
-      <NavLink className="auth-btn" to="/profile/edit">Update user settings</NavLink>
+      <NavLink className="profile-edit-btn" to="/profile/edit">Update user settings</NavLink>
     </div>
   )
 }
