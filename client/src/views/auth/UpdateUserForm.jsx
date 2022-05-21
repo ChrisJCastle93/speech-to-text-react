e34d54e3cd4408357b1ad209bbd2cd50fc953aa5
cschnitzel10 <boese.caroline@gmail.com>
2022-05-21 10:27:47 +0200

    stylingggg

--- client/src/views/auth/UpdateUserForm.jsx
+++ client/src/views/auth/UpdateUserForm.jsx
@@ -9,26 +9,26 @@ import "../../css/editProfile.css";
 export const UpdateUserForm = (props) => {
   // const navigate = useNavigate();
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const id =props.loggedInUser._id
-  console.log("props",props);
+  const id = props.loggedInUser._id
+  console.log("props", props);
   const onSubmit = async (data) => {
 
-  const res=  await apiService.update(data.username, data.password,id)
+    const res = await apiService.update(data.username, data.password, id)
     props.setLoggedInUser(res)
   };
 
   console.log(props.loggedInUser?.username)
   return (
     <container>
-    <div className="img-container">
-      <img className="prof-img" src ={profileLamp} alt="lamp-setting" />
+      <div className="img-container">
+        <img className="prof-img" src ={profileLamp} alt="lamp-setting" />
       <img className="prof-img-2" src ={lamp2} alt="lamp-setting" />
       </div>
         <form className="edit-credentials" onSubmit={handleSubmit(onSubmit)}>
           <label className="label" for="profileEdit">Edit your username:</label>
-          <input 
-          // defaultValues={props.loggedInUser.username}
-          defaultValue={props.loggedInUser?.username}
+          <input
+            // defaultValues={props.loggedInUser.username}
+            defaultValue={props.loggedInUser?.username}
             {...register("username", {
               // required: "Please enter a valid username",
             })}
@@ -36,10 +36,10 @@ export const UpdateUserForm = (props) => {
             name="username"
           />
           <p>{errors.username?.message}</p>
-            
+
           <label className="label" for="profileEdit">Change your password:</label>
-          <input type="password" 
-          defaultValue={props.loggedInUser?.password}
+          <input type="password"
+            defaultValue={props.loggedInUser?.password}
             {...register("password", {
               // required: "This is required",
               minLength: {
@@ -53,6 +53,7 @@ export const UpdateUserForm = (props) => {
           <p>{errors.password?.message}</p>
           <button className="btn" type="submit">Save changes</button>
         </form>
-      </container>
+ 
+    </container>
   );
 };
